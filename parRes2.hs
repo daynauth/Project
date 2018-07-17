eea1588ca1ef5d3c90db1edcb1bfcc17edcf9872
Roland Daynauth <daynauth@gmail.com>
2018-07-17 11:59:57 +0100

    added the third method

--- parRes2.hs
+++ parRes2.hs
@@ -5,10 +5,9 @@ import Control.Monad
 import Control.Applicative
 import System.Environment
 import Poly
-
-
-type Vec = [Ratio Integer]
-type Matrix = [Vec]
+import Control.Parallel 
+import Control.Parallel.Strategies 
+import Control.DeepSeq
 
 sMatrix :: Poly -> Poly -> Matrix
 sMatrix u v = sHMatrix u (degreeGPE v) ++ sHMatrix v (degreeGPE u)
@@ -18,10 +17,23 @@ sHMatrix p n = [replicate x 0 ++ r ++ replicate (d - x) 0 | x <- [0 .. d]]
                 where r = reverse p
                       d = n - 1
 
+parSMatrix :: Poly -> Poly -> Matrix
+parSMatrix u v = runEval $ do 
+      as <- rpar(sHMatrix u (degreeGPE v))
+      bs <- rpar(sHMatrix v (degreeGPE u))
+      rseq as
+      rseq bs
+      return (as ++ bs)
+
 determinant :: Matrix -> Coef
 determinant [[x]] = x
 determinant mat = sum [let x = (head mat) !! i; s = (-1)^i in s*x*(determinant (getRest i mat)) | i <- [0..n-1]]
                 where n = length $ head mat
+
+parDeterminant :: Matrix -> Coef
+parDeterminant mat = sum cofactor
+                  where cofactor = [(-1)^i * x * determinant (getRest i mat) |  (i, x) <- zip [0.. n - 1] (head mat)] `using` parList rdeepseq
+                        n = length $ head mat
 
 getRest :: Int -> Matrix -> Matrix
 getRest i mat = removeCols i (tail mat)
@@ -32,7 +44,12 @@ removeCols i (r:rs) = (left ++ (tail right)) : removeCols i rs
                 where n = length r
                       (left, right) = splitAt i r
 
-resultant' :: Poly -> Poly -> Coef
-resultant' u v = determinant $ sMatrix u v
 
-main = print $ resultant' [1,2,3,345,67,345,345,343,484,6884,4,7,88,78895, 4554,548,68,487,693,-4854,2,3] [4554,548,68,487,693,-4854,2,3,1,2,3,345,67,345,345,343,484,6884]
+parResultant' :: Poly -> Poly -> Coef
+parResultant' u v = parDeterminant $ sMatrix u v
+
+resultant' :: Poly -> Poly -> Coef
+resultant' u v = determinant $ parSMatrix u v
+
+--main = print $ parResultant' [1,2,3,345,67,345,345,343,484,6884,4,7,88,78895, 4554,548,68,487,693,-4854,2,3] [4554,548,68,487,693,-4854,2,3,1,2,3,345,67,345,345,343,484,6884]
+main = print $ parResultant' [1,2,3,345,67,345,343] [67,693,2,3, 5, 1]
